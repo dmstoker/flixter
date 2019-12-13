9f355953e9da6fed5e8e5bdc3b1b31261566b0ff
dmstoker <dammitdylan88@gmail.com>
2019-12-13 22:43:49 +0000

    Video Uplouding

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,10 +1,10 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class VideoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-   include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  #storage :file
    storage :aws
 
   # Override the directory where uploaded files will be stored.
@@ -21,10 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-   #Process files as they are uploaded:
-
-   process resize_to_fill: [800, 350]
-
+  # Process files as they are uploaded:
   # process scale: [200, 300]
   #
   # def scale(width, height)
