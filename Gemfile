d75ca6862d6d21ca301ef57134b3c320fc9a68dd
dmstoker <dammitdylan88@gmail.com>
2019-12-14 08:35:36 +0000

    adding stripe and configuring cc input

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem "figaro"
 gem 'fog-aws'
 gem "mini_magick"
 gem 'carrierwave-aws'
+gem 'stripe'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use postgresql as the database for Active Record
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,22 @@
 class LessonsController < ApplicationController
-  def show
-  end
+    before_action :authenticate_user!
+    before_action :require_user_be_enrolled, only: [:show]
 
-  private
+    def show
+    end
 
-  helper_method :current_lesson
-   def current_lesson
-     @current_lesson ||= Lesson.find(params[:id])
-   end
+    private
+
+    helper_method :current_lesson
+
+    def current_lesson
+        @current_lesson ||= Lesson.find(params[:id])
+    end
+
+    def require_user_be_enrolled
+        if !current_user.enrolled_in?(current_lesson.section.course)
+            redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled in this course before you can view its lessons.'
+        end
+    end
+
 end
